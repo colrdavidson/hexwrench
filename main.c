fd4300d46e1d5ea02712ab9f061b273d2461616d
Colin Davidson <colrdavidson@gmail.com>
2025-05-20 13:39:15 -0700

    add terminal pretties, remove copy buffer, take user input

--- main.c
+++ main.c
@@ -1,12 +1,189 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <termios.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/ioctl.h>
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+char term_buf[32] = {};
+void enable_altbuffer(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[?1049h");
+    write(0, term_buf, len);
+}
+void disable_altbuffer(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[?1049l");
+    write(0, term_buf, len);
+}
+void save_term(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[?47h");
+    write(0, term_buf, len);
+}
+void restore_term(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[?47l");
+    write(0, term_buf, len);
+}
+void clear_term(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[2J");
+    write(0, term_buf, len);
+}
+void reset_cursor(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[H");
+    write(0, term_buf, len);
+}
+void set_cursor(int row, int col) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[%d;%dH", row, col);
+    write(0, term_buf, len);
+}
+void erase_line(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[2K");
+    write(0, term_buf, len);
+}
+void set_foreground(int color) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[38;5;%dm", color);
+    write(0, term_buf, len);
+}
+void set_background(int color) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[48;5;%dm", color);
+    write(0, term_buf, len);
+}
+void reset_color(void) {
+    int len = snprintf(term_buf, sizeof(term_buf), "\x1b[0m");
+    write(0, term_buf, len);
+}
+
+typedef struct {
+    char *name;
+    char *data;
+    uint64_t size;
+} File;
+
+void print_block(File file, uint64_t buffer_size, uint64_t offset) {
+    if (((int64_t)file.size - (int64_t)offset) <= 0) {
+        printf("no bytes to display!\n");
+        return;
+    }
+
+    uint64_t rem_file_size = file.size - offset;
+    uint64_t read_size = buffer_size;
+    if (rem_file_size < buffer_size) {
+        read_size = rem_file_size % buffer_size;
+    }
+
+    char *buffer = file.data + offset;
+
+    uint64_t chunk_size = 16;
+    for (int i = 0; i < (buffer_size / chunk_size); i++) {
+        uint64_t sub_idx = i * chunk_size;
+
+        uint8_t *row = (uint8_t *)buffer + sub_idx;
+        if (sub_idx >= read_size) {
+            break;
+        }
+
+        printf("\x1b[38;5;248m%08llx\x1b[0m: ", offset + sub_idx);
+
+        for (int j = 0; j < chunk_size; j++) {
+            if (sub_idx + j >= read_size) {
+                printf("   ");
+                continue;
+            }
+
+            printf("%02x ", row[j]);
+        }
+        printf(" \x1b[38;5;248m");
+        for (int j = 0; j < chunk_size; j++) {
+            if (sub_idx + j >= read_size) {
+                printf("   ");
+                continue;
+            }
+
+            char ch = row[j];
+            if (ch == '\n' || ch == '\t') {
+                ch = '.';
+            }
+            printf("%c", ch);
+        }
+        printf("\x1b[0m\n");
+
+        if (i >= read_size / chunk_size) {
+            break;
+        }
+    }
+
+    return;
+}
+
+
+
+struct termios orig_termios;
+void cleanup_term(void) {
+    tcsetattr(0, TCSAFLUSH, &orig_termios);
+    disable_altbuffer();
+}
+void init_term(void) {
+    enable_altbuffer();
+
+    tcgetattr(0, &orig_termios);
+    atexit(cleanup_term);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(0, TCSAFLUSH, &raw);
+}
+
+typedef struct {
+    uint64_t rows;
+    uint64_t cols;
+} Window;
+
+bool get_term_size(Window *w) {
+    struct winsize ws;
+    if (ioctl(1, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return false;
+    }
+
+    w->rows = ws.ws_row - 1;
+    w->cols = ws.ws_col;
+    return true;
+}
+
+typedef struct {
+    File file;
+    Window w;
+
+    uint64_t block_size;
+    uint64_t offset;
+} ViewState;
+ViewState view;
+
+void refresh_screen(void) {
+    clear_term();
+    reset_cursor();
+
+    set_background(244);
+    set_foreground(232);
+    erase_line();
+
+    printf(" %s\n", view.file.name);
+
+    reset_color();
+    print_block(view.file, (view.w.rows - 1) * 16, view.offset);
+    set_cursor(view.w.rows + 1, 1);
+}
+
+void handle_sigwinch(int unused) {
+    get_term_size(&view.w);
+    refresh_screen();
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -33,56 +210,31 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    uint64_t block_size = 64;
-    uint64_t offset = 0x0;
+    init_term();
 
-    char *buffer = malloc(block_size);
+    view = (ViewState){
+        .file = (File){.name = argv[1], .data = file_bytes, .size = file_size},
+        .offset = 0
+    };
+    get_term_size(&view.w);
+    signal(SIGWINCH, handle_sigwinch);
 
-    uint64_t rem_file_size = file_size - offset;
-    uint64_t read_size = block_size;
-    if (rem_file_size < block_size) {
-        read_size = rem_file_size % block_size;
-    }
-    uint64_t zero_size = block_size - read_size;
+    for (;;) {
+        refresh_screen();
 
-    memcpy(buffer, file_bytes + offset, read_size);
-    memset(buffer + read_size, 0, zero_size);
+        char ch;
+        read(0, &ch, 1);
 
-    uint64_t chunk_size = 16;
-    for (int i = 0; i < (block_size / chunk_size); i++) {
-        uint64_t sub_idx = i * chunk_size;
-
-        uint8_t *row = (uint8_t *)buffer + sub_idx;
-        if (sub_idx >= read_size) {
-            break;
-        }
-
-        printf("%08llx: ", offset + sub_idx);
-        for (int j = 0; j < chunk_size; j++) {
-            if (sub_idx + j >= read_size) {
-                printf("   ");
-                continue;
-            }
-
-            printf("%02x ", row[j]);
-        }
-        printf("\t");
-        for (int j = 0; j < chunk_size; j++) {
-            if (sub_idx + j >= read_size) {
-                printf("   ");
-                continue;
-            }
-
-            char ch = row[j];
-            if (ch == '\n' || ch == '\t') {
-                ch = '.';
-            }
-            printf("%c", ch);
-        }
-        printf("\n");
-
-        if (i >= read_size / chunk_size) {
-            break;
+        switch (ch) {
+            case 'w': {
+                view.offset = MAX((int64_t)(view.offset - 16), 0);
+            } break;
+            case 's': {
+                view.offset = MIN(view.offset + 16, view.file.size);
+            } break;
+            case 'q': {
+                return 1;
+            } break;
         }
     }
 }
